7a34418c7887d0e63ad49e9c904d098aad485982
sp184218 <sp184218@gmail.com>
2025-08-14 09:48:32 -0700

    added the food page cuz I was bored lol and havev time

---
+++ frontend/src/components/ui/badge.jsx
@@ -0,0 +1,8 @@
+// src/components/ui/badge.jsx
+export function Badge({ children, className }) {
+  return (
+    <span className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ${className}`}>
+      {children}
+    </span>
+  );
+}
