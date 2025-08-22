a1f6c99efe1d35443a4183793a6556686527edd2
sp184218 <sp184218@gmail.com>
2025-08-22 12:50:05 -0700

    updated another workout issue to make it capital W

--- frontend/src/LogWorkoutPage.jsx
+++ frontend/src/LogWorkoutPage.jsx
@@ -12,7 +12,7 @@ import {
 } from "./components/ui/card";
 import { Badge } from "./components/ui/badge";
 import { useAuth } from "./AuthContext";
-import "./workout.css";
+import "./Workout.css";
 
 export default function LogWorkoutPage() {
   const navigate = useNavigate();
